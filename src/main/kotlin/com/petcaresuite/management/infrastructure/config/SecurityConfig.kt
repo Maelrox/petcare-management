0ae21c4e7a1f8e1d5f5e4fcfd90908ebc2fbd5de
Morgox <oscarquiroz@gmail.com>
2024-08-24 09:17:25 -0500

    Allow any origin (cors) when spring starts using dev profile

--- src/main/kotlin/com/petcaresuite/management/infrastructure/config/SecurityConfig.kt
+++ src/main/kotlin/com/petcaresuite/management/infrastructure/config/SecurityConfig.kt
@@ -1,10 +1,11 @@
 package com.petcaresuite.management.infrastructure.config
 
-import com.petcaresuite.management.infrastructure.security.UserDetailsService
 import com.petcaresuite.management.infrastructure.security.JwtAuthFilter
+import com.petcaresuite.management.infrastructure.security.UserDetailsService
 import jakarta.servlet.DispatcherType
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider
@@ -18,13 +19,17 @@ import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.security.web.util.matcher.DispatcherTypeRequestMatcher
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig(
     private val authFilter: JwtAuthFilter,
     private val customUserDetailsService: UserDetailsService,
-    )  {
+    private val environment: Environment
+)  {
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
@@ -41,6 +46,7 @@ class SecurityConfig(
                 sessionCreationPolicy = SessionCreationPolicy.STATELESS
             }
             addFilterBefore<UsernamePasswordAuthenticationFilter>(authFilter)
+            cors{ }
             authenticationProvider()
         }
         return http.build()
@@ -62,6 +68,21 @@ class SecurityConfig(
     @Throws(Exception::class)
     fun authenticationManager(config: AuthenticationConfiguration): AuthenticationManager {
         return config.authenticationManager
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        if (environment.activeProfiles.contains("dev")) {
+            configuration.allowedOrigins = listOf("*")
+        } else {
+            configuration.allowedOrigins = listOf("https://petcaresuite.com") // Replace with your production origin
+        }
+        configuration.applyPermitDefaultValues()
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 
 }
