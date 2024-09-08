4ef3c3274c45bbb93b0d8e6643292f113aff9116
Morgox <oscarquiroz@gmail.com>
2024-09-08 06:44:09 -0500

    cors commented it will be handled by gateway port dynamic to 0 and eureka client configuration

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,4 @@ WORKDIR /app
 
 COPY build/libs/management-0.1.jar management.jar
 
-EXPOSE 8181
-
 ENTRYPOINT ["java", "-jar", "management.jar"]
--- src/main/kotlin/com/petcaresuite/management/infrastructure/config/SecurityConfig.kt
+++ src/main/kotlin/com/petcaresuite/management/infrastructure/config/SecurityConfig.kt
@@ -5,7 +5,6 @@ import com.petcaresuite.management.infrastructure.security.UserDetailsService
 import jakarta.servlet.DispatcherType
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.env.Environment
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider
@@ -19,17 +18,13 @@ import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.security.web.util.matcher.DispatcherTypeRequestMatcher
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig(
     private val authFilter: JwtAuthFilter,
-    private val customUserDetailsService: UserDetailsService,
-    private val environment: Environment
-)  {
+    private val customUserDetailsService: UserDetailsService
+) {
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
@@ -46,12 +41,12 @@ class SecurityConfig(
                 sessionCreationPolicy = SessionCreationPolicy.STATELESS
             }
             addFilterBefore<UsernamePasswordAuthenticationFilter>(authFilter)
-            cors{ }
             authenticationProvider()
         }
         return http.build()
     }
 
+    @Bean
     fun authenticationProvider(): AuthenticationProvider {
         val authProvider = DaoAuthenticationProvider()
         authProvider.setUserDetailsService(customUserDetailsService)
@@ -69,20 +64,77 @@ class SecurityConfig(
     fun authenticationManager(config: AuthenticationConfiguration): AuthenticationManager {
         return config.authenticationManager
     }
-
-    @Bean
-    fun corsConfigurationSource(): CorsConfigurationSource {
-        val configuration = CorsConfiguration()
-        if (environment.activeProfiles.contains("dev")) {
-            configuration.allowedOrigins = listOf("*")
-        } else {
-            configuration.allowedOrigins = listOf("https://petcaresuite.com") // Replace with your production origin
-        }
-        configuration.applyPermitDefaultValues()
-        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
-        val source = UrlBasedCorsConfigurationSource()
-        source.registerCorsConfiguration("/**", configuration)
-        return source
-    }
-
 }
+
+/**
+ * NOTE: GATEWAY WILL TAKE CARE ABOUT CORS IF NOT GATEWAY NEEDED USE THIS TESTED CONFIG
+ *
+ * package com.petcaresuite.management.infrastructure.config
+ *
+ * import com.petcaresuite.management.infrastructure.security.JwtAuthFilter
+ * import com.petcaresuite.management.infrastructure.security.UserDetailsService
+ * import jakarta.servlet.DispatcherType
+ * import org.springframework.context.annotation.Bean
+ * import org.springframework.context.annotation.Configuration
+ * import org.springframework.security.authentication.AuthenticationManager
+ * import org.springframework.security.authentication.AuthenticationProvider
+ * import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+ * import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+ * import org.springframework.security.config.annotation.web.builders.HttpSecurity
+ * import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+ * import org.springframework.security.config.annotation.web.invoke
+ * import org.springframework.security.config.http.SessionCreationPolicy
+ * import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+ * import org.springframework.security.crypto.password.PasswordEncoder
+ * import org.springframework.security.web.SecurityFilterChain
+ * import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+ * import org.springframework.security.web.util.matcher.DispatcherTypeRequestMatcher
+ *
+ * @Configuration
+ * @EnableWebSecurity
+ * class SecurityConfig(
+ *     private val authFilter: JwtAuthFilter,
+ *     private val customUserDetailsService: UserDetailsService
+ * ) {
+ *
+ *     @Bean
+ *     fun filterChain(http: HttpSecurity): SecurityFilterChain {
+ *         http {
+ *             authorizeRequests {
+ *                 authorize(DispatcherTypeRequestMatcher(DispatcherType.FORWARD), permitAll)
+ *                 authorize(DispatcherTypeRequestMatcher(DispatcherType.ERROR), permitAll)
+ *                 authorize("/authentication/**", permitAll)
+ *                 authorize("/user/register", permitAll)
+ *                 authorize(anyRequest, authenticated)
+ *             }
+ *             csrf { disable() }
+ *             sessionManagement {
+ *                 sessionCreationPolicy = SessionCreationPolicy.STATELESS
+ *             }
+ *             addFilterBefore<UsernamePasswordAuthenticationFilter>(authFilter)
+ *             authenticationProvider()
+ *         }
+ *         return http.build()
+ *     }
+ *
+ *     @Bean
+ *     fun authenticationProvider(): AuthenticationProvider {
+ *         val authProvider = DaoAuthenticationProvider()
+ *         authProvider.setUserDetailsService(customUserDetailsService)
+ *         authProvider.setPasswordEncoder(passwordEncoder())
+ *         return authProvider
+ *     }
+ *
+ *     @Bean
+ *     fun passwordEncoder(): PasswordEncoder {
+ *         return BCryptPasswordEncoder()
+ *     }
+ *
+ *     @Bean
+ *     @Throws(Exception::class)
+ *     fun authenticationManager(config: AuthenticationConfiguration): AuthenticationManager {
+ *         return config.authenticationManager
+ *     }
+ * }
+ *
+ */
